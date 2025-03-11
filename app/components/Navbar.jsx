5c146e2e7aae0a29d1823650bc81ea02b49f9261
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2025-03-11 09:50:36 +0100

    Navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { ArrowUpRight, ChevronDown } from 'lucide-react';
+import { ArrowUpRight, ChevronDown, Search } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react'
 
@@ -9,18 +9,16 @@ const Navbar = () => {
             <div className='bg-dark-green p-2 text-center'>
                 <h1 className='text-white font-thin'>Toll-free emergency lines (LASEMA) 767 | 112</h1>
             </div>
-            <nav className="flex gap-6">
+            <nav className="flex gap-6 justify-between p-6 border-b">
                 <div>
-                    <img src="logo.png" alt="" />
+                    <img src="logo.png" className='h-[50px] w-[50px]' alt="" />
                 </div>
-                <div>
+                <div className='flex gap-6 items-center'>
                     <Link href="#" className="flex items-center gap-1">
                         HOME <ArrowUpRight size={16} />
                     </Link>
-                    <Link href="#">
-                        <div className="flex items-center gap-1 cursor-pointer">
-                            GOVERNMENT <ChevronDown size={16} />
-                        </div>
+                    <Link href="#" className="flex items-center gap-1">
+                        GOVERNMENT <ChevronDown size={16} />
                     </Link>
                     <Link href="#" className="flex items-center gap-1">
                         SERVICES <ArrowUpRight size={16} />
@@ -34,6 +32,9 @@ const Navbar = () => {
                     <Link href="#" className="flex items-center gap-1">
                         CONNECT <ArrowUpRight size={16} />
                     </Link>
+                </div>
+                <div>
+                    <Search />
                 </div>
             </nav>
         </>
