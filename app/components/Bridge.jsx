1de889d0e300d978e7f7fa96b05a2ed2a9614d53
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2025-03-11 16:48:23 +0100

    bridge

--- app/components/Bridge.jsx
+++ app/components/Bridge.jsx
@@ -2,36 +2,36 @@ import React from 'react';
 
 const Bridge = () => {
     return (
-        <section className='flex justify-between items-center gap-6 border-t border-gray-300 py-4'>
-            <div className='text-center'>
-                <h3 className='font-light uppercase text-sm tracking-wide'>Est. Population</h3>
+        <section className='flex justify-between items-center gap-6 border-gray-300 p-12'>
+            <div className='text-center space-y-4'>
+                <h3 className='font-light uppercase text-sm '>Est. Population</h3>
                 <p className='font-bold text-2xl'>22mn+</p>
             </div>
 
             <Divider />
 
-            <div className='text-center'>
-                <h3 className='font-light uppercase text-sm tracking-wide'>Number of Visitors</h3>
+            <div className='text-center space-y-4'>
+                <h3 className='font-light uppercase text-sm '>Number of Visitors</h3>
                 <p className='font-bold text-2xl'>1.3mn+</p>
             </div>
 
             <Divider />
 
-            <div className='text-center'>
-                <h3 className='font-light uppercase text-sm tracking-wide'>State Revenue - 2023</h3>
+            <div className='text-center space-y-4'>
+                <h3 className='font-light uppercase text-sm '>State Revenue - 2023</h3>
                 <p className='font-bold text-2xl'>N651.2bn</p>
             </div>
 
             <Divider />
 
-            <div className='text-center'>
+            <div className='text-center space-y-4'>
                 <h3 className='font-light uppercase text-sm tracking-wide'>Vehicles on the Road</h3>
                 <p className='font-bold text-2xl'>5.2mn+</p>
             </div>
 
             <Divider />
 
-            <div className='text-center'>
+            <div className='text-center space-y-4'>
                 <h3 className='font-light uppercase text-sm tracking-wide'>Biggest Economy</h3>
                 <p className='font-bold text-2xl'>6th in Africa</p>
             </div>
