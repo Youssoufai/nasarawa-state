f10b8c7dbb4ba6a5709a2faf0e18750ac36e4ee4
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2025-03-23 06:19:41 +0100

    about page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 const Page = () => {
     return (
         <>
-            <h2>About Page</h2>
+            <section className="relative h-screen bg-[url('/governor.jpeg')] bg-center bg-cover">
+                {/* Dark Overlay */}
+                <div className="absolute inset-0 bg-black/70"></div>
+
+                {/* Content Goes Here */}
+                <div className="relative z-10 flex items-center justify-center h-full text-white">
+                    <h1 className="text-3xl font-bold">Welcome</h1>
+                </div>
+            </section>
         </>
-    )
-}
+    );
+};
 
 export default Page;
